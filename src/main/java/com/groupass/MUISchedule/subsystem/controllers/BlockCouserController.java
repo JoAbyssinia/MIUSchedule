8f7e658cf62579aee942234370ffba96ac82cad1
bmlindelwa <3441209@myuwc.ac.za>
2022-03-14 22:53:30 -0500

    added controllers

---
+++ src/main/java/com/groupass/MUISchedule/subsystem/controllers/BlockCouserController.java
@@ -0,0 +1,31 @@
+package com.groupass.MUISchedule.subsystem.controllers;
+
+import com.groupass.MUISchedule.subsystem.entities.Block_Course;
+import com.groupass.MUISchedule.subsystem.services.IBlock_CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/blockcourses")
+public class BlockCouserController {
+
+    @Autowired
+    private IBlock_CourseService blockCourseService;
+
+    @GetMapping
+    public List<Block_Course> getAll(){
+        return blockCourseService.getAllBlockCourses();
+    }
+
+    @PostMapping
+    public Block_Course save(@RequestBody Block_Course course){
+        return blockCourseService.save(course);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteBlockCourseById(@PathVariable Long id){
+        blockCourseService.deleteById(id);
+    }
+}
